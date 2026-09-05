2909998856aa13e32d50e991e2de2f6509f34cda
Алексей Маврин <alexeymavrin@yandex.ru>
2026-09-05 08:29:24 +0300

    Подключен swagger. Создан шаблон контроллера

---
+++ HappyBooking_Ya/Controllers/EventController.cs
@@ -0,0 +1,40 @@
+﻿using HappyBooking_Ya.models;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace HappyBooking_Ya.Controllers
+{
+    public class EventController : ControllerBase
+    {
+        [HttpGet]
+        public void Get()
+        {
+            
+        }
+
+        [HttpGet("{id: int}")]
+        public void Get(int id)
+        {
+            
+        }
+
+        [HttpPost("{event: Event}")]
+        public void Create([FromBody] Event newEvent)
+        {
+            
+        }
+
+        [HttpPut("{id: int}, {updatedEvent: Event}")]
+        public void Put(int id, [FromBody] Event updatedEvent)
+        {
+            
+        }
+
+        [HttpDelete("{id: int}")]
+        public void Delete(int id)
+        {
+            
+        }
+    }
+}
+
